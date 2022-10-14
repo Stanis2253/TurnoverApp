15d7043b8e5a20c51b1db3da4374a5d9746d867a
Stanis2253 <91283273+Stanis2253@users.noreply.github.com>
2022-10-14 08:19:37 +0300

    Основной каркас готов

---
+++ CustomElementControl/ComboboxInput.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomElementControl
+{
+    /// <summary>
+    /// Логика взаимодействия для ComboboxInput.xaml
+    /// </summary>
+    public partial class ComboboxInput : UserControl
+    {
+        public string Text { get; set; }
+        public ComboboxInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
